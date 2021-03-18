028ecb035abb232193a70bee81a6f01b07cdeaf8
Shubham Sharma <60926838+shubham763@users.noreply.github.com>
2021-03-18 10:59:07 +0530

    Update MainActivity.java

--- chapter 11/Access Scope This And Static/MainActivity.java
+++ chapter 11/Access Scope This And Static/MainActivity.java
@@ -26,24 +26,24 @@ public class MainActivity extends AppCompatActivity {
         //girlship.shieldStrength = 999;
 
         //But we have a public getter
-        Log.i("girlShip shieldStrngth: ", "" + girlShip.getShieldStrength());
-        Log.i("boyShip shieldStrngth: ", "" + boyShip.getShieldStrength());
+        Log.i("girlShip shields: ", "" + girlShip.getShieldStrength());
+        Log.i("boyShip shields: ", "" + boyShip.getShieldStrength());
 
         //But we can't do this because it's private
         //boyship.setShieldStrength(1000000);
 
         //lets shoot some ships
         girlShip.hitDetected();
-        Log.i("girlShip shieldStrngth: ", "" + girlShip.getShieldStrength());
-        Log.i("boyShip shieldStrngth: ", "" + boyShip.getShieldStrength());
+        Log.i("girlShip shields: ", "" + girlShip.getShieldStrength());
+        Log.i("boyShip shields: ", "" + boyShip.getShieldStrength());
         boyShip.hitDetected();
         boyShip.hitDetected();
         boyShip.hitDetected();
-        Log.i("girlShip shieldStrngth: ", "" + girlShip.getShieldStrength());
-        Log.i("boyShip shieldStrngth: ", "" + boyShip.getShieldStrength());
+        Log.i("girlShip shields: ", "" + girlShip.getShieldStrength());
+        Log.i("boyShip shields: ", "" + boyShip.getShieldStrength());
         boyShip.hitDetected();//ahhh
-        Log.i("girlShip shieldStrngth: ", "" + girlShip.getShieldStrength());
-        Log.i("boyShip shieldStrngth: ", "" + boyShip.getShieldStrength());
+        Log.i("girlShip shields: ", "" + girlShip.getShieldStrength());
+        Log.i("boyShip shields: ", "" + boyShip.getShieldStrength());
         Log.i("numShips: ", "" + AlienShip.getNumShips());
     }
 
